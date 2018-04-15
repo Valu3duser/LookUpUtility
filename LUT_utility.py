0279db7ee86e9249e8b652b77e4d2a9bcb5bf375
Dave Renzo <dave.p.renzo@gmail.com>
2018-04-15 19:35:13 -0400

    added gui code

--- LUT_utility.py
+++ LUT_utility.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pylab as plt
 from scipy import signal
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 
-num_samples = 256
+
 bit_depth = {   '8 bit' : 255,
                 '10 bit': 1023,
                 '12 bit': 4095,
@@ -101,20 +104,83 @@ class ExpTable(LookUpTable):
 
 #################### main() #################################################    
 if __name__ == "__main__":
-    lut = ExpTable(num_samples,1024)
-    lut.plot_exp()
-    #lut.generate_avr_header('tri.h')
-    #lut.generate_csv('tri.csv')
+
+    filename = None
+    ver_num = '0.0.1'
+
+    root = tk.Tk()
+    root.title('Look Up Table Utility '+ver_num)
+    root.geometry('270x160')
+    bit_depth_tupple = ('8 bit','10 bit','12 bit','16 bit')
+    num_samples = ('32','64','128','256','512','1024')
+    wave_shapes = ('Sine','Expo 1', 'Expo 2', 'Triangle', 'Up Saw', 'Down Saw')
+    num_samples_choice = tk.StringVar()
+    bit_dept_choice = tk.StringVar()
+    wave_choice = tk.StringVar()
+    num_samples_choice.set('256')
+    bit_dept_choice.set('8 bit')
+    wave_choice.set('Sine')
+
+    def open_dialog():
+        global filename
+        if v.get() == 1:
+            filename = filedialog.asksaveasfilename(filetypes =(("csv files","*.csv"),),defaultextension = ".csv")
+        else:
+            filename = filedialog.asksaveasfilename(filetypes =(("C Header files","*.h"),),defaultextension = ".h")
+        output_dir_label.config(text = filename)  
+
+    def start():
+        messagebox.askyesno(message = num_samples_choice.get())
+        #print('Hi')
+
+    
+
+    f2 = tk.Frame(root)
+    bd_label = tk.Label(f2, text = 'Bit Depth' )
+    bd_label.grid(column = 0, row = 0, sticky='E')
+    
+    
+    opt1 = tk.OptionMenu(f2, bit_dept_choice, *bit_depth_tupple)
+    opt1.grid(column = 1, row =0, sticky ='EW')
+    opt1.config(width = 18)
+
+    
+    sample_label = tk.Label(f2, text = 'Number of Samples' )
+    sample_label.grid(column = 0, row = 1, sticky='E')
+    opt2 = tk.OptionMenu(f2, num_samples_choice, *num_samples)
+    opt2.config(width = 18)
+    opt2.grid(column =1, row =1, sticky ='EW')
+
+    wave_label = tk.Label(f2, text = 'Wave Shape')
+    wave_label.grid(column = 0, row = 2, sticky='E')
+    opt3 = tk.OptionMenu(f2, wave_choice, *wave_shapes)
+    opt3.config(width = 18)
+    opt3.grid(column =1, row =2, sticky ='EW')
+
+    #f3 = tk.Frame(root)
+    #output_label = tk.Label(f3, text = 'Ouput file')
+    #output_label.grid(column = 0, row = 2, sticky='W')
+    #f3.grid(column= 0, row = 1)
+
+    #output_dir_label = tk.Label(f3)
+    #output_dir_label.grid(column = 1, row = 2, sticky='W')
+    f2.grid(column =0 , row = 0, sticky = 'W', pady =3)
+    f1 = tk.Frame(root)
+    output_button = tk.Button(f1, text='Choose output location', command = open_dialog)
+    output_button.grid(column =0, row =3)
+
+    v = tk.IntVar()
+    v.set(2)
+
+    radio_1 = tk.Radiobutton(f1,text ='csv', variable = v, value = 1)
+    radio_1.grid(column = 1, row = 3)
+
+    radio_2 = tk.Radiobutton(f1,text ='avr header', variable = v, value = 2)
+    radio_2.grid(column = 2, row = 3)
+    f1.grid(column = 0, row = 2, sticky = 'W')
+
+    start_button = tk.Button(f1, text = 'Create LUT', command = start)
+    start_button.grid(column =0, row = 4, columnspan = 3, sticky ='EW',pady =3)
 
 
-    #env_linear = np.arange(0, 257.0) / 256.0
-
-    #env_expo = np.exp(4*env_linear)
-
-    #env_expo_norm = []
-
-    #for i in env_expo:
-    #    env_expo_norm.append(i/env_expo[255])
-
-    #plt.plot(env_linear, env_expo_norm)
-    #plt.show()
+    root.mainloop()
